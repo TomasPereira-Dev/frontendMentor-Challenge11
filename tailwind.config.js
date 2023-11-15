fcf00faeaf81c95f78bbd4a7921da49f3a7d7573
Tomas Pereira <tomaspereira2k@gmail.com>
2023-11-15 18:16:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [    
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "custom-cyan": {
+          DEFAULT: "hsl(172, 67%, 45%)",
+          100: "hsl(189, 41%, 97%)",
+          200: "hsl(185, 41%, 84%)",
+          300: "hsl(184, 14%, 56%)",
+          400: "hsl(186, 14%, 43%)",
+          500: "hsl(183, 100%, 15%)"
+        }
+      },
+      fontFamily: {
+        "mono": "'Space Mono', monospace"
+      },
+      maxWidth: {
+        "data-per-person": "10ch"
+      }
+    },
+  },
+  plugins: [],
+}
+
